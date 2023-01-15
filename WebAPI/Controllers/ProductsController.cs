7fad397d12d9daf3d8bf089873a4d4b9ea5af33a
Çağatay KOÇ <kocagatayy@gmail.com>
2023-01-15 20:13:12 +0300

    .

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //ATTRIBUTE -- Bir class'la ilgili bilgi verme imzama yöntemi?? Araştır...
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled -- gevşek bağlılık
+        //namming convention -- örn: altçizgi ile isimlendirilmesi
+        //IoC(Inversion of Control) Container -- aşağıdaki contstruction'a ereşemediği için bir trick
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+                
+        }
+
+
+    }
+}
