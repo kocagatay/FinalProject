f6482a555e888dbde768b95575c928322b9d6af6
Çağatay KOÇ <kocagatayy@gmail.com>
2023-03-05 14:49:33 +0300

    Backend(Final)

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,9 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Category> Categories { get; set; } // Category nesnemi Categories'e bağla.
         public DbSet<Customer> Customers { get; set; } // Customer nesnemi Customers'a bağla.
         public DbSet<Order> Orders { get; set; } // Order nesnemi Orders'a bağla.
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
 
     }
